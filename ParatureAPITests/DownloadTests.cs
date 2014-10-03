6b4bc2c33c013aac9c23e0dd4740e87ecf7bfe72
Adam Hong <ahong@parature.com>
2014-10-03 09:24:32 -0400

    Moved APITests folder and contents

---
+++ ParatureAPITests/DownloadTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParatureAPI;
+
+namespace ParatureAPITests
+{
+    [TestClass]
+    public class DownloadTests
+    {
+        /// <summary>
+        /// Schema should indicate that only one folder is allowed
+        /// </summary>
+        [TestMethod]
+        public void DownloadXmlParserSingleFolder()
+        {
+            var creds = TestCredentials.Credentials;
+            creds.Departmentid = 45021;
+
+            var download = ApiHandler.Download.DownloadSchema(creds);
+
+            bool FlagSetCorrectly = !download.MultipleFolders;
+
+            Assert.IsTrue(FlagSetCorrectly);
+        }
+
+        /// <summary>
+        /// Confirm that XML parser correctly sets the flag and that this file has multiple folders
+        /// </summary>
+        [TestMethod]
+        public void DownloadXmlParserMultipleFolders()
+        {
+            var creds = TestCredentials.Credentials;
+            creds.Departmentid = 45001;
+
+            var Download = ApiHandler.Download.DownloadGetDetails(1, creds);
+
+            bool FlagSetCorrectly = Download.MultipleFolders;
+
+            Assert.IsTrue(FlagSetCorrectly && (Download.Folders.Count > 1));
+        }
+
+        /// <summary>
+        /// Tests that adding multiple folders to a Download file that does not allow multiple folders throws the correct exception
+        /// </summary>
+        [TestMethod]
+        public void TestFoldersException()
+        {
+            var download = new ParaObjects.Download(false);
+            download.Folders.Add(new ParaObjects.DownloadFolder());
+            download.Folders.Add(new ParaObjects.DownloadFolder());
+
+            bool caughtException = false;
+
+            try
+            {
+                ApiHandler.Download.DownloadInsert(download, TestCredentials.Credentials);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                caughtException = true;
+            }
+
+            Assert.IsTrue(caughtException);
+        }
+
+    }
+}
---
+++ ParatureAPITests/TestCredentials.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using ParatureAPI;
+
+namespace ParatureAPITests
+{
+    static class TestCredentials
+    {
+        public static ParaCredentials Credentials
+        {
+            get
+            {
+                return new ParaCredentials("1uYL8hcXDySYSXhkQJDwqnjTzrDC2ZxdMG8JkCASeNYmn@GLw@Q2Iotdp12cvvAGq8o7l2FADj2GjNR8JHpLug==", "https://demo.parature.com/", Paraenums.ApiVersion.v1, 45001, 45021, false);
+            }
+        }
+    }
+}
