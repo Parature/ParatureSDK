ef81db7082a238dad58a4d04d73eb6489e59785b
adhong <adam.hong@microsoft.com>
2014-11-21 16:05:03 -0500

    Added Ticket_Sla to Ticket Object

---
+++ ParatureAPITests/TicketTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParatureAPI;
+
+namespace ParatureAPITests
+{
+    [TestClass]
+    public class TicketTests
+    {
+        [TestMethod]
+        public void TicketSlaParsing()
+        {
+            var creds = TestCredentials.Credentials;
+            creds.Departmentid = 45001;
+            var ticket = ApiHandler.Ticket.TicketGetDetails(1016, false, creds);
+
+            Assert.IsTrue(ticket.Ticket_Sla.SlaID != 0);
+        }
+    }
+}
